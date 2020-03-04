7cbd50586a89c9bc8a146379bed0146246b23d97
Palani Kumanan <palani.kumanan@gmail.com>
2020-03-04 16:09:10 -0500

    using chai for test

--- test/node-es6-starter.spec.js
+++ test/node-es6-starter.spec.js
@@ -1,28 +1,46 @@
-import hello from '../src/index.js';
-
-let chai = require('chai');
-
-chai.should();
+import { expect } from 'chai';
+import { exec } from 'child_process';
+import hello from '../src';
 
 function grabStdout() {
-  const write = process.stdout.write;
+  const { write } = process.stdout;
   let content = '';
   process.stdout.write = (chunk, encoding, cb) => {
     content += chunk.toString();
     return write(chunk, encoding, cb);
-  }
+  };
 
   return {
     letGo: () => {
-        process.stdout.write = write;
+      process.stdout.write = write;
     },
-    content: content,
+    content,
   };
 }
 
 describe('Hello', () => {
-  it('Print Default', () => {
-    const greetings = hello();
-    greetings.should.eql('Hello World!\n');
-  })
+  describe('Function return', () => {
+    it('Return Default', () => {
+      const greetings = hello();
+      expect(greetings).to.equal('Hello World!\n');
+    });
+
+    it('Return Param', () => {
+      const greetings = hello('Friend');
+      expect(greetings).to.equal('Hello Friend!\n');
+    });
+  });
+
+  describe('stdout', () => {
+    it('Print Default', (done) => {
+      exec('npx babel-node src/', (error, stdout) => {
+        if (error) {
+          done(error);
+          return;
+        }
+        expect(stdout).to.equal('Hello World!\n');
+        done();
+      });
+    }).timeout(5000);
+  });
 });
